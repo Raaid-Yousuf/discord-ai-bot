5149db9cfac3f72ebba2be3094bb1a51cc459d64
Raaid Yousuf <raaid.yousuf1019@gmail.com>
2026-05-01 17:08:24 +0500

    Initial commit - Discord AI Bot using Groq

---
+++ main.py
@@ -0,0 +1,58 @@
+# app id
+# 1499713605443129384
+
+# public key
+# 52ab2aa75e5676c40923e8473f9565468dc623f2121a6b4ffd0dc757092c3884
+
+import discord
+from dotenv import load_dotenv
+import os
+
+chat =""
+
+load_dotenv()
+token =os.getenv("SECRET_KEY")
+
+import os
+
+from groq import Groq
+
+groq_client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        global chat
+        chat += f"{message.author}: {message.content}\n"
+        print(f'Message from {message.author}: {message.content}')
+        if self.user!= message.author:
+            if self.user in message.mentions:
+                channel =message.channel
+
+
+                chat_completion = groq_client.chat.completions.create(
+                    messages=[
+                        {
+                            "role": "user",
+                            "content": f"{chat}\nRaaidGPT: ",
+                        }
+                    ],
+                    model="llama-3.3-70b-versatile",
+                    )
+
+                messageToSend= chat_completion.choices[0].message.content
+                await channel.send(messageToSend)
+
+
+
+intents = discord.Intents.default()
+intents.message_content= True
+
+discord_client = MyClient(intents=intents)
+discord_client.run(token)
